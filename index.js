434622a4b025fe019c532551083b636614073ee3
rimiakter4 <rimia9941@gmail.com>
2025-12-12 07:29:45 +0600

    connected mongodb

--- index.js
+++ index.js
@@ -1,10 +1,55 @@
 const express =require("express")
 const app =express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors=require("cors")
 require('dotenv').config()
 const port =process.env.PORT||3000
 app.use(cors())
 app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q32hk1x.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+const db=client.db("germents_factory_db")
+
+const productsCollection=db.collection("products")
+const usersCollection=db.collection("users")
+
+
+// user api
+
+ 
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
